e16df3f960fb14db116bf37e99a2a78fcf6da31c
Jeff Kerns <jkerns@gmx.com>
2025-11-08 15:36:26 -0500

    1.3 Component Interaction & Data Flow

--- src/app/hello-jeffy/hello-jeffy.ts
+++ src/app/hello-jeffy/hello-jeffy.ts
@@ -1,13 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hello-jeffy',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './hello-jeffy.html',
   styleUrl: './hello-jeffy.scss',
 })
 export class HelloJeffy implements OnInit {
+
+  coolname = 'Earl'; // change to mess with Jeffy
+  paragraphtext = 'This is the inner text in that paragraph tag.  This can be for any html attribute.';
+  showDetails = false;
+  items = ['Apple Fritters', 'Oatmeal Cookies', 'Pumpkin Rolls'];
+  detailsBtnText = 'Show Details'
+
   ngOnInit(): void {
     console.log('ngOnInit called');
+  }
+
+  changeName() {
+    if (this.coolname === 'Earl') {
+      this.coolname = 'Jeffy';
+    } else {
+      this.coolname = 'Earl';
+    }
+  }
+
+  toggleDetails() {
+    this.showDetails = !this.showDetails;
+    if (this.showDetails) {
+      this.detailsBtnText = 'Hide Details';
+    }else{
+      this.detailsBtnText = 'Show Details';
+    }
   }
 }
