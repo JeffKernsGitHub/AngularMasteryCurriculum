dd7867008ff5ddf5c8d89f57c9aef19073aed9e7
Jeff Kerns <jkerns@gmx.com>
2025-11-20 18:51:34 -0500

    Update to better outline scoped DI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,13 @@ import { Routes } from '@angular/router';
 import { SignalsExampleComponent } from './signals-example/signals-example.component';
 import {DiServicesComponent} from './di-services/di-services.component';
 
+
 export const routes: Routes = [
   { path: 'di-example', component: DiServicesComponent },
-  { path: 'signals-example', component: SignalsExampleComponent }
+  { path: 'signals-example', component: SignalsExampleComponent },
+  {
+    path: 'di-demo',
+    // For standalone, you can directly load the component
+    loadComponent: () => import('./di-demo-page/di-demo-page.component').then(m => m.DiDemoPageComponent)
+  },
 ];
