95d64f037dfafbd8783a13ab8cb59f786135795a
Jeff Kerns <jkerns@gmx.com>
2025-11-20 16:56:39 -0500

    update to Angular 21

--- src/app/hello-jeffy/hello-jeffy.ts
+++ src/app/hello-jeffy/hello-jeffy.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-hello-jeffy',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule],
   templateUrl: './hello-jeffy.html',
   styleUrl: './hello-jeffy.scss',
 })
